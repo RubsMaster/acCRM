5b89d7b19881d9afddb43f01abd5e51778dfbd65
rubsmaster <jrubsb@gmail.com>
2025-07-30 17:05:51 -0600

    cambiecitos

--- src/controllers/clients.controller.js
+++ src/controllers/clients.controller.js
@@ -41,7 +41,7 @@ export const getAllClients = async (req, res) => {
   try {
     const clients = await Client.find();
 
-    res.status(200).json({data: clients});
+    res.status(200).json({data: clients.reverse()});
   } catch (error) {
     res.status(500).json({ message: 'Error al obtener los clientes', error });
   }
