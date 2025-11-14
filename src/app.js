6779528375404c479dbd0082d6a47771971347fc
rubsmaster <jrubsb@gmail.com>
2025-11-14 17:02:07 -0600

    apply cors to origin all

--- src/app.js
+++ src/app.js
@@ -1,18 +1,22 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import {connectDB} from "./config/mongo-db.js";
-import clientsRoutes from './routes/clients.routes.js';
-import servicesRoutes from './routes/services.routes.js';
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import { connectDB } from "./config/mongo-db.js";
+import clientsRoutes from "./routes/clients.routes.js";
+import servicesRoutes from "./routes/services.routes.js";
 
 const app = express();
 dotenv.config();
 connectDB();
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
-app.use('/api/acCRM/clients',clientsRoutes);
-app.use('/api/acCRM/services',servicesRoutes)
+app.use("/api/acCRM/clients", clientsRoutes);
+app.use("/api/acCRM/services", servicesRoutes);
 
-export default app
+export default app;
